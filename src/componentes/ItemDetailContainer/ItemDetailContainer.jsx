10d078650d145b2d8397b90c83e6cabeeb818a2c
Daniel Quijano <hector.daniel.quijano@gmail.com>
2023-08-17 11:45:11 -0300

    ddd

--- src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,22 +1,14 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { obtenerProducto } from "../../services/MockService";
 import ItemDetail from "./ItemDetail";
 import { useParams } from "react-router-dom";
-import { cartContext } from "../../storage/cartContext";
 import Loader from "../Loader/Loeader";
 
 function ItemDetailContainer() {
     const [producto, setProducto] = useState({ title: "Cargando", precio: "-" });
     const [isLoading, setIsLoading] = useState(true);
-    const [isInCart, setIsInCart] = useState(false)
 
     let params = useParams();
-    const { addToCart } = useContext(cartContext);
-
-    function handleAddToCart(count) {
-        setIsInCart(true)
-        addToCart({ ...producto, count: count });
-    }
 
     useEffect(() => {
         obtenerProducto(params.itemid)
@@ -35,7 +27,7 @@ function ItemDetailContainer() {
                 isLoading ? (
                     <Loader></Loader>
                 ) : (
-                    <ItemDetail isInCart={isInCart} onAddToCart={handleAddToCart} title={producto.title} img={producto.img} detalle={producto.detalle} precio={producto.precio} stock={producto.stock} />
+                    <ItemDetail title={producto.title} img={producto.img} detalle={producto.detalle} precio={producto.precio} stock={producto.stock} />
                 )
             }
         </>
